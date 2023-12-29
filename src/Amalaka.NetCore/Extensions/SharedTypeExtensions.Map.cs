aafa164e7802162d77e3c27ebef9b92356231c90
HUMAOSHUAI <hu15150@126.com>
2023-12-29 13:44:13 +0800

    add method

--- src/Amalaka.NetCore/Extensions/SharedTypeExtensions.Map.cs
+++ src/Amalaka.NetCore/Extensions/SharedTypeExtensions.Map.cs
@@ -2,93 +2,69 @@
 
 namespace System.Linq;
 
-public static partial class QueryableExtensions
+public static partial class IQExtensions
 {
-    private static readonly ConcurrentDictionary<(Type TSource, Type TResult), Delegate> _typeMappingCache = [];
+    private static readonly ConcurrentDictionary<(Type TSource, Type TResult), LambdaExpression> _typeMappingCache = [];
 
-    private static readonly ConcurrentDictionary<(Type TOuter, Type TInner, Type TResult), Delegate> _typeJoinMappingCache = [];
+    private static readonly ConcurrentDictionary<(Type TOuter, Type TInner, Type TResult), LambdaExpression> _typeJoinMappingCache = [];
 
-    public static TResult Map<TSource, TResult>(this TSource source) where TSource : class where TResult : class
+    public static Expression<Func<TSource, TResult>> Map<TSource, TResult>(this TSource _) where TSource : class where TResult : class
     {
         var lambdaExpression = _typeMappingCache.GetOrAdd((typeof(TSource), typeof(TResult)), (valueFactory) =>
         {
-            List<MemberBinding> memberBindings = [];
-            ParameterExpression parameterExpression = Expression.Parameter(valueFactory.TSource, "p0");
-            foreach (var property in valueFactory.TResult.GetProperties())
+            var typeMapping = new Dictionary<string, (Type Type, ParameterExpression ParameterExpression)>
             {
-                var propertyName = property.Name;
-                var declaringTypeName = valueFactory.TSource!.Name;
-                if (property.TryGetCustomAttribute<MapFromAttribute>(out var attribute))
-                {
-                    propertyName = attribute!.Name.Replace(".", "");
-                }
-                var splitWords = propertyName.SplitCapitalLetters();
-                if (splitWords.Length > 1)
-                {
-                    propertyName = splitWords[1];
-                    declaringTypeName = splitWords[0];
-                }
-                if (!declaringTypeName.Equals(valueFactory.TSource.Name, StringComparison.Ordinal))
-                {
-                    continue;
-                }
-                if (!valueFactory.TSource.TryGetMember(propertyName, out var memberInfo))
-                {
-                    continue;
-                }
-                var memberExpression = Expression.MakeMemberAccess(parameterExpression, memberInfo!);
-                memberBindings.Add(Expression.Bind(property, memberExpression));
-            }
-            var memberInitExpression = Expression.MemberInit(Expression.New(valueFactory.TResult), memberBindings);
-            return Expression.Lambda<Func<TSource, TResult>>(memberInitExpression, parameterExpression).Compile();
+                { valueFactory.TSource.Name, (valueFactory.TSource, Expression.Parameter(valueFactory.TSource,"p0")) },
+            };
+            var memberInitExpression = MemberInit(valueFactory, typeMapping);
+            return Expression.Lambda<Func<TSource, TResult>>(memberInitExpression, typeMapping.Select(s => s.Value.ParameterExpression));
         });
-        return ((Func<TSource, TResult>)lambdaExpression).Invoke(source);
+        return (Expression<Func<TSource, TResult>>)lambdaExpression;
     }
 
-    public static TResult Map<TSource, TInner, TResult>(this TSource outer, TInner inner) where TSource : class where TResult : class
+    public static Expression<Func<TSource, TInner, TResult>> Map<TSource, TInner, TResult>(this TSource _) where TSource : class where TInner : class where TResult : class
     {
-        var resultSelector = _typeJoinMappingCache.GetOrAdd((typeof(TSource), typeof(TInner), typeof(TResult)), (valueFactory) =>
+        var lambdaExpression = _typeJoinMappingCache.GetOrAdd((typeof(TSource), typeof(TInner), typeof(TResult)), (valueFactory) =>
         {
-            var typeMapping = new Dictionary<string, (Type Type, ParameterExpression parameterExpression)>
+            var typeMapping = new Dictionary<string, (Type Type, ParameterExpression ParameterExpression)>
             {
                 { valueFactory.TOuter.Name, (valueFactory.TOuter, Expression.Parameter(valueFactory.TOuter,"p0")) },
                 { valueFactory.TInner.Name, (valueFactory.TInner, Expression.Parameter(valueFactory.TInner,"p1")) }
             };
-
-            List<MemberBinding> memberBindings = [];
-            var parameterExpressions = new List<ParameterExpression>();
-            foreach (var propertyInfo in valueFactory.TResult.GetProperties())
-            {
-                var propertyName = propertyInfo.Name;
-                var declaringTypeName = valueFactory.TOuter!.Name;
-                if (propertyInfo.TryGetCustomAttribute<MapFromAttribute>(out var attribute))
-                {
-                    propertyName = attribute!.Name.Replace(".", "");
-                }
-                var splitWords = propertyName.SplitCapitalLetters();
-                if (splitWords.Length > 1)
-                {
-                    propertyName = splitWords[1];
-                    declaringTypeName = splitWords[0];
-                }
-                if (!typeMapping.TryGetValue(declaringTypeName, out var mapping))
-                {
-                    continue;
-                }
-                if (!mapping.Type.TryGetMember(propertyName, out var memberInfo))
-                {
-                    continue;
-                }
-                var memberExpression = Expression.MakeMemberAccess(mapping.parameterExpression, memberInfo!);
-                memberBindings.Add(Expression.Bind(propertyInfo, memberExpression));
-                if (!parameterExpressions.Contains(mapping.parameterExpression))
-                {
-                    parameterExpressions.Add(mapping.parameterExpression);
-                }
-            }
-            var memberInitExpression = Expression.MemberInit(Expression.New(valueFactory.TResult), memberBindings);
-            return Expression.Lambda<Func<TSource, TInner, TResult>>(memberInitExpression, parameterExpressions).Compile();
+            var memberInitExpression = MemberInit((valueFactory.TOuter, valueFactory.TResult), typeMapping);
+            return Expression.Lambda<Func<TSource, TInner, TResult>>(memberInitExpression, typeMapping.Values.Select(s => s.ParameterExpression));
         });
-        return ((Func<TSource, TInner, TResult>)resultSelector!).Invoke(outer, inner);
+        return (Expression<Func<TSource, TInner, TResult>>)lambdaExpression;
+    }
+
+    private static MemberInitExpression MemberInit((Type TOuter, Type TResult) valueFactory, Dictionary<string, (Type Type, ParameterExpression ParameterExpression)> typeMapping)
+    {
+        List<MemberBinding> memberBindings = [];
+        foreach (var propertyInfo in valueFactory.TResult.GetProperties())
+        {
+            var propertyName = propertyInfo.Name;
+            var declaringTypeName = valueFactory.TOuter!.Name;
+            if (propertyInfo.TryGetCustomAttribute<MapFromAttribute>(out var attribute))
+            {
+                propertyName = attribute!.Name.Replace(".", "");
+            }
+            var splitWords = propertyName.SplitCapitalLetters();
+            if (splitWords.Length > 1)
+            {
+                propertyName = splitWords[1];
+                declaringTypeName = splitWords[0];
+            }
+            if (!typeMapping.TryGetValue(declaringTypeName, out var mapping))
+            {
+                continue;
+            }
+            if (!mapping.Type.TryGetMember(propertyName, out var memberInfo))
+            {
+                continue;
+            }
+            var memberExpression = Expression.MakeMemberAccess(mapping.ParameterExpression, memberInfo!);
+            memberBindings.Add(Expression.Bind(propertyInfo, memberExpression));
+        }
+        return Expression.MemberInit(Expression.New(valueFactory.TResult), memberBindings);
     }
 }
