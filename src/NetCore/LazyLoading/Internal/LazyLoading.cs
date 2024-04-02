94acd3e2560fbd2b88ee441883002ce992aa865f
HUMAOSHUAI <hu15150@126.com>
2024-04-02 17:25:50 +0800

    restore

--- src/NetCore/LazyLoading/Internal/LazyLoading.cs
+++ src/NetCore/LazyLoading/Internal/LazyLoading.cs
@@ -1,16 +1,5 @@
 ﻿namespace Microsoft.Extensions.DependencyInjection.Internal;
 
-public class LazyLoading<T> : Lazy<T>, ILazyLoading<T> where T : notnull
+public class LazyLoading<T>(IServiceProvider serviceProvider) : Lazy<T>(serviceProvider.GetRequiredService<T>), ILazyLoading<T> where T : notnull
 {
-
-    public LazyLoading(IServiceProvider serviceProvider) : base(serviceProvider.GetRequiredService<T>)
-    {
-        var tt = 11111;
-    }
-
-
-    ~LazyLoading()
-    {
-        var t = 1;
-    }
 }
