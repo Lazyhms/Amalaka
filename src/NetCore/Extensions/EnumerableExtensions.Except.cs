cd2a7ab3f3a219c48cbf92e8a7f77dcfc6ff252a
HUMAOSHUAI <hu15150@126.com>
2024-06-24 16:00:59 +0800

    add new method

---
+++ src/NetCore/Extensions/EnumerableExtensions.Except.cs
@@ -0,0 +1,10 @@
+﻿namespace System.Collections.Generic;
+
+public static partial class EnumerableExtensions
+{
+    public static IEnumerable<TSource> ExceptBy<TSource, TKey>(this IEnumerable<TSource> first, IEnumerable<TSource> second, Func<TSource, TKey> keySelector)
+        => ExceptBy(first, second, keySelector, null);
+
+    public static IEnumerable<TSource> ExceptBy<TSource, TKey>(this IEnumerable<TSource> first, IEnumerable<TSource> second, Func<TSource, TKey> keySelector, IEqualityComparer<TKey>? comparer)
+        => first.ExceptBy(second.Select(keySelector), keySelector, comparer);
+}
---
+++ src/NetCore/Extensions/EnumerableExtensions.Intersect.cs
@@ -0,0 +1,10 @@
+﻿namespace System.Collections.Generic;
+
+public static partial class EnumerableExtensions
+{
+    public static IEnumerable<TSource> IntersectBy<TSource, TKey>(this IEnumerable<TSource> first, IEnumerable<TSource> second, Func<TSource, TKey> keySelector)
+        => IntersectBy(first, second, keySelector, null);
+
+    public static IEnumerable<TSource> IntersectBy<TSource, TKey>(this IEnumerable<TSource> first, IEnumerable<TSource> second, Func<TSource, TKey> keySelector, IEqualityComparer<TKey>? comparer)
+        => first.IntersectBy(second.Select(keySelector), keySelector, comparer);
+}
